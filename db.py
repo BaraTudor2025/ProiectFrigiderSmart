06aca87a5dbf9289c5656cfc4d11a4c365ec7dba
Bara C. Tudor <vector2025@gmail.com>
2022-01-30 17:45:34 +0200

    adauga test conexiune mongo

--- db.py
+++ db.py
@@ -10,13 +10,18 @@ logger = logging.getLogger('mongo-db')
 
 # object is a singleton
 g_frigider_db: Database = None
+g_mongo_client: MongoClient = None
 
 def get_database() -> Database:
     global g_frigider_db
+    global g_mongo_client
     if g_frigider_db == None:
         client = MongoClient("mongodb+srv://florian:florian@smartfridgecluster.yle9m.mongodb.net")
         logger.debug('client connection made')
         atexit.register(lambda: client.close())
         g_frigider_db = client['frigider-db']
+        g_mongo_client = client
     return g_frigider_db
 
+def close_db_connection():
+    g_mongo_client.close()
--- test/test_http.py
+++ test/test_http.py
@@ -3,6 +3,7 @@ import pytest
 import json
 from main import create_app
 from flask.testing import FlaskClient
+from db import get_database, close_db_connection
 
 @pytest.fixture()
 def client() -> FlaskClient:
@@ -11,9 +12,15 @@ def client() -> FlaskClient:
     client = local_app.test_client()
     yield client
 
+def test_db_connection():
+    db = get_database()
+    assert db != None
+    close_db_connection()
+
 def test_register(client: FlaskClient):
     rv = client.post('/auth/register', data={'username': 'admin', 'password': 'admin'}, follow_redirects=True)
     assert rv.status_code == 403
     res = json.loads(rv.data.decode())
     assert res['status'] == 'User already exists'
+
 
