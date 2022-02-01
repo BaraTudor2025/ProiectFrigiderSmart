f2e4f6f9a6e74b45940e5051816b40049621c6e8
Bara C. Tudor <vector2025@gmail.com>
2022-02-01 21:09:17 +0200

    codul pentru inchiderea usii, mai trebuie testat

--- door.py
+++ door.py
@@ -1,28 +1,64 @@
-import db
+from flask import g
 from flask_mqtt import Mqtt
 from main import mqtt
 
 """
 flow:
  - clientul(usa/frigiderul): daca usa e deschisa, o data pe secunda, 
-    trimite unghiul la care usa este deschisa {angle:<nr-in-grade(0,90)>}
- - server: daca trec 5 secunde AND unghiul ramane acelasi +- un epsilon AND < 15 grade
+    trimite unghiul la care usa este deschisa {intre 0 si 90, unde 0 este inchis}
+ - server: (valori-default) daca trec 3 secunde AND unghiul ramane acelasi +- un epsilon(=1.0) AND < 15 grade
     atunci inchide usa
-    SAU daca > 15 si trec 5 minute inchide usa
+ - clientul poate modifica unghiul de inchidere cu o valoare intre 15, 45 si numarul de secunde de asteptat (intre 3 si 15)
+    la topicul /door/set/angle si /door/set/seconds
 
- - clientul: publish la o secunda /python/mqtt/opendoor - 'angle:<nr>'
- - server: subscribe la /python/mqtt/opendoor - 'angle:<nr>'
+ - clientul: publish la o secunda la /door/angle: <angle>
+ - server: subscribe la /door/angle: <angle>
 
- - client: subscribe la /opendoor - 'close'
- - server: publish la /opendoor - 'close'
-
- - foloseste session['door_time'] pentru contor
+ - payloadul mesajului este irelevant, prin faptul ca am transmis la topicul /door/close usa se inchide
+ - client: subscribe la /door/close, 
+ - server: publish la /door/close
 """
 
-def start_timer():
+angle_to_close = 15
+seconds_to_wait = 3
+
+def mqtt_subscribe():
     def handle(client, userdata, msg):
-        if msg.topic == 'python/mqtt/opendoor':
-            str = msg.payload.decode()
-        pass
-    mqtt.on_message(handle)
-    mqtt.subscribe('python/mqtt/opendoor')
+        global angle_to_close, seconds_to_wait
+        if msg.topic == '/door/angle':
+            newAngle = float(msg.payload.decode())
+            angles: list = g.get('angles')
+            if angles == None:
+                g['angles'] = [newAngle]
+            else:
+                angles.append(newAngle)
+                # se presupuna ca distanta intre mesaje este de 1 secunda, deci len == time
+                if len(angles) > seconds_to_wait:
+                    angles.pop(0)
+                if len(angles) == seconds_to_wait:
+                    # sa fie toate sub <angle_to_close>
+                    for a in angles:
+                        if a > angle_to_close:
+                            return
+                    # eroarea de masurare pt float (daca exista) sa fie sub 1 grad
+                    dif = 0
+                    for i in range(0, len(angles) - 1):
+                        dif += (angles[i] - angles[i+1])
+                    if abs(dif) < 1:
+                        mqtt.publish('/door/close', True, qos=2)
+                        g.pop('angles')
+
+        elif msg.topic == '/door/set/angle':
+            angle = int(msg.payload.decode())
+            if angle >= 15 and angle <= 45:
+                angle_to_close = angle
+        elif msg.topic == '/door/set/seconds':
+            sec = int(msg.payload.decode())
+            if sec >= 3 and sec <= 15:
+                seconds_to_wait = sec
+
+    mqtt.on_message = handle
+    mqtt.subscribe('/door/angle', qos=2)
+    mqtt.subscribe('/door/set/angle', qos=2)
+    mqtt.subscribe('/door/set/seconds', qos=2)
+
--- main.py
+++ main.py
@@ -33,13 +33,13 @@ def create_app():
 
 def create_mqtt_app():
     global mqtt
-    app.config['MQTT_BROKER_URL'] = 'localhost'  # use the free broker from HIVEMQ
-    app.config['MQTT_BROKER_PORT'] = 5000 #1883  # default port for non-tls connection
+    app.config['MQTT_BROKER_URL'] = 'broker.hivemq.com'  # use the free broker from HIVEMQ
+    app.config['MQTT_BROKER_PORT'] = 1883  # default port for non-tls connection
     app.config['MQTT_USERNAME'] = ''  # set the username here if you need authentication for the broker
     app.config['MQTT_PASSWORD'] = ''  # set the password here if the broker demands authentication
     app.config['MQTT_KEEPALIVE'] = 5  # set the time interval for sending a ping to the broker to 5 seconds
     app.config['MQTT_TLS_ENABLED'] = False  # set TLS to disabled for testing purposes
-    #mqtt = Mqtt(app)
+    mqtt = Mqtt(app)
     #thread = Thread(target=mqtt_thread, args=(mqtt, app))
     #thread.daemon = True
     #thread.start()
