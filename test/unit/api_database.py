8d5748470fb6d8c89d828c5327089918127abaf9
Neagu Andrei-Alexandru <58010312+Neagu-Andrei@users.noreply.github.com>
2022-02-02 02:23:49 +0200

    Create api_database.py

---
+++ test/unit/api_database.py
@@ -0,0 +1,23 @@
+import pymongo.errors
+from db import get_database, close_db_connection
+import logging
+
+log = logging.getLogger()
+
+
+def check_database_connection():
+    db = get_database()
+    if db is None:
+        log.error("Couldn't establish database connection")
+        return False
+    else:
+        return True
+
+
+def close_database_connection():
+    try:
+        close_db_connection()
+        return True
+    except pymongo.errors as e:
+        log.error(e)
+        return False
